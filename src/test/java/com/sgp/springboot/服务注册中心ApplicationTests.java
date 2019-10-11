5c37fa77872af21d6932b3c4681ee7874b3abc78
sgpisme <1065257045@qq.com>
2019-10-11 16:15:15 +0800

    first commit

---
+++ src/test/java/com/sgp/springboot/服务注册中心ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sgp.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class 服务注册中心ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
